d51e6a14207c173510c853e22923a13f64f8794d
plus7 <plus7@8ec09257-eed5-4793-a7f5-9088093d7eb7>
2008-07-20 15:26:15 +0000

    splitted CSS related functions to StyleSheetUtils.pas
    git-svn-id: http://plus7.unfuddle.com/svn/plus7_bagel@91 8ec09257-eed5-4793-a7f5-9088093d7eb7

--- Legacy/Bagel/UBagel.pas
+++ Legacy/Bagel/UBagel.pas
@@ -22,6 +22,7 @@ uses
   GeckoSimpleProfile, //Profile
   GeckoSimpleHALDialog, //Helper Application Dialog
   GeckoSimplePrompt, //Prompt Service
+  StyleSheetUtils, //CSS関連のユーティリティー関数
 
   ActiveX,          //OleInitialize
   buinIntelliMouse, //マウス関連の定数
@@ -1352,88 +1353,6 @@ begin
     nsMemory.Free(selstr);
   end;
 end;
-
-function IsValidForFrame(Frame: nsIDOMWindow; Title: String):Boolean;
-var
-  i:Integer;
-  ssl:nsIDOMStyleSheetList;
-  styleSheet:nsIDOMStyleSheet;
-  str:IInterfacedstring;
-begin
-  Result:=False;
-  ssl := (frame.Document as nsIDOMDocumentStyle).StyleSheets;
-
-  str:=NewString;
-
-  if Title='_nostyle' then Result:=True
-  else if TiTle='_default' then Result:=True
-  else
-  begin
-    For i:=0 to ssl.Length -1 do
-    begin
-      styleSheet:=ssl.Item(i);
-      styleSheet.GetTitle(str.AString);
-      if str.ToString = Title then
-      begin
-        Result:=True;
-        exit;
-      end;
-    end;
-  end;
-end;
-
-function IsValidMedia(StyleSheet:nsIDOMStyleSheet):Boolean;//media=screen のみ true
-var
-  mediaText:IInterfacedString;
-  strMedia:String;
-begin
-  MediaText:=NewString('');
-  StyleSheet.Media.GetMediaText(mediaText.AString);
-  strMedia:=LowerCase(mediatext.ToString);
-  Result:=False;
-  if (strMedia='') or (Pos('all',strMedia)<>0) or (Pos('screen',strMedia)<>0) then Result:=true;
-end;
-
-function IsAlternate(StyleSheet:nsIDOMStyleSheet):Boolean;
-var
-  Node:nsIDOMNode;
-  NodeName:IInterfacedString;
-  Attr:IInterfacedString;
-  Rel:IInterfacedString;
-begin
-  Result:=False;
-  Node := styleSheet.OwnerNode;
-  if Node=nil then Exit;
-  NodeName:=NewString;
-  Node.GetNodeName(NodeName.AString);
-  if LowerCase(NodeName.ToString)<>'link' then Exit;
-  Rel:=NewString('rel');
-  Attr:=NewString('');
-  (Node as nsIDOMElement).GetAttribute(Rel.AString,Attr.AString);
-  if Attr.ToString = 'alternate stylesheet' then Result:=True;
-end;
-
-function IsPersistent(StyleSheet:nsIDOMStyleSheet):Boolean;
-var
-  title:IInterfacedString;
-begin
-  title:=NewString;
-  styleSheet.GetTitle(title.AString);
-	Result := (not isAlternate(styleSheet)) and (title.ToString = '');
-end;
-function IsPreferred(StyleSheet:nsIDOMStyleSheet):Boolean;
-var
-  title:IInterfacedString;
-begin
-  title:=NewString;
-  styleSheet.GetTitle(title.AString);
-  Result := (not isAlternate(styleSheet)) and (title.ToString <> '');
-end;
-procedure SetStyleSheetDisabled(StyleSheet:nsIDOMStyleSheet; IsDisabled:Boolean);
-begin
-  StyleSheet.SetDisabled(IsDisabled);
-end;
-
 
 function URIToStr(aURI:nsIURI):String;
 var

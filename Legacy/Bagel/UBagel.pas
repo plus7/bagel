a6a1e73d8cc7753d0edb2c006027364ce7714a74
plus7 <plus7@8ec09257-eed5-4793-a7f5-9088093d7eb7>
2008-07-20 15:47:32 +0000

    splitted nsIDOMWindow related functions to WindowUtils.pas
    git-svn-id: http://plus7.unfuddle.com/svn/plus7_bagel@95 8ec09257-eed5-4793-a7f5-9088093d7eb7

--- Legacy/Bagel/UBagel.pas
+++ Legacy/Bagel/UBagel.pas
@@ -24,6 +24,7 @@ uses
   GeckoSimplePrompt, //Prompt Service
   StyleSheetUtils, //CSS関連のユーティリティー関数
   DOMUtils, //DOM関連のユーティリティー関数
+  WindowUtils, //nsIDOMWindow関連のユーティリティー関数
 
   ActiveX,          //OleInitialize
   buinIntelliMouse, //マウス関連の定数
@@ -1218,80 +1219,7 @@ begin
 			Result := Result + ToolBar.Buttons[i].Width;
 end;
 
-function GetDocShellObjByWin(win:nsIDOMWindow):nsIDocShell;
-var
-  doc:nsIDOMDocument;
-  ir:nsIInterfaceRequestor;
-  webnav:nsiWebnavigation;
-  docshell:nsIDocShell;
-begin
-  if win=nil then exit;
-  doc:=win.Document;
-  (doc as nsIDOMDocumentView).GetDefaultView.QueryInterface(nsIInterfaceRequestor,ir);
-  ir.GetInterface(nsiWebnavigation,webnav);
-  webNav.QueryInterface(nsIDocShell,docshell);
-  Result:=docshell;
-end;
 
-function GetSelConByWin(win:nsIDOMWindow):nsISelectionController;
-var
-  ir:nsIInterfaceRequestor;
-  seldisp:nsISelectionDisplay;
-  selcon:nsISelectionController;
-begin
-  GetDocShellObjByWin(win).QueryInterface(nsIInterfaceRequestor,ir);
-  ir.GetInterface(nsISelectionDisplay,seldisp);
-  seldisp.QueryInterface(nsISelectionController,selcon);
-  Result:=selcon;
-end;
-
-function GetRootWindowByWin(win:nsIDOMWindow):nsIDOMWindow;
-var
-  tmpWin,window:nsIDOMWindow;
-begin
-  Result:=nil;
-  if win=nil then Exit;
-  tmpWin := win.Parent;
-  while tmpWin<>nil do begin
-    if window=tmpWin then break;
-    window:=tmpWin;
-    tmpWin := window.Parent;
-  end;
-  Result:=window;
-end;
-
-function GetSelectionStrFromWin(win:nsIDOMWindow):WideString;
-var
-  childwin:nsIDOMWindow;
-  sel:nsISelection;
-  length:Cardinal;
-  i:Integer;
-  ret:String;
-  selstr:PWideChar;
-begin
-  Result:='';
-  if win=nil then Exit;
-  sel := win.GetSelection;
-  if sel.IsCollapsed then begin
-    length := win.Frames.Length;
-    if length=0 then Result:=''
-    else begin
-      for i:=0 to length-1 do begin
-        childwin := win.Frames.Item(i);
-        ret:=GetSelectionStrFromWin(childwin);
-        if ret<>'' then begin
-          Result:=ret;
-          exit;
-        end;
-      end;
-    end;
-  end
-  else begin
-    selstr := sel.ToString;
-    Result := WideString(selstr);
-    nsMemory.Free(selstr);
-  end;
-end;
 
 function URIToStr(aURI:nsIURI):String;
 var
--- Legacy/Bagel/WindowUtils.pas
+++ Legacy/Bagel/WindowUtils.pas
@@ -2,6 +2,88 @@ unit WindowUtils;
 
 interface
 
+uses SysUtils, nsMemory, nsXPCOM, nsTypes, nsGeckoStrings, nsDocShell, nsSelection, BrowserSupports;
+
+function GetDocShellObjByWin(win:nsIDOMWindow):nsIDocShell;
+function GetSelConByWin(win:nsIDOMWindow):nsISelectionController;
+function GetRootWindowByWin(win:nsIDOMWindow):nsIDOMWindow;
+function GetSelectionStrFromWin(win:nsIDOMWindow):WideString;
+
 implementation
+
+function GetDocShellObjByWin(win:nsIDOMWindow):nsIDocShell;
+var
+  doc:nsIDOMDocument;
+  ir:nsIInterfaceRequestor;
+  webnav:nsiWebnavigation;
+  docshell:nsIDocShell;
+begin
+  if win=nil then exit;
+  doc:=win.Document;
+  (doc as nsIDOMDocumentView).GetDefaultView.QueryInterface(nsIInterfaceRequestor,ir);
+  ir.GetInterface(nsiWebnavigation,webnav);
+  webNav.QueryInterface(nsIDocShell,docshell);
+  Result:=docshell;
+end;
+
+function GetSelConByWin(win:nsIDOMWindow):nsISelectionController;
+var
+  ir:nsIInterfaceRequestor;
+  seldisp:nsISelectionDisplay;
+  selcon:nsISelectionController;
+begin
+  GetDocShellObjByWin(win).QueryInterface(nsIInterfaceRequestor,ir);
+  ir.GetInterface(nsISelectionDisplay,seldisp);
+  seldisp.QueryInterface(nsISelectionController,selcon);
+  Result:=selcon;
+end;
+
+function GetRootWindowByWin(win:nsIDOMWindow):nsIDOMWindow;
+var
+  tmpWin,window:nsIDOMWindow;
+begin
+  Result:=nil;
+  if win=nil then Exit;
+  tmpWin := win.Parent;
+  while tmpWin<>nil do begin
+    if window=tmpWin then break;
+    window:=tmpWin;
+    tmpWin := window.Parent;
+  end;
+  Result:=window;
+end;
+
+function GetSelectionStrFromWin(win:nsIDOMWindow):WideString;
+var
+  childwin:nsIDOMWindow;
+  sel:nsISelection;
+  length:Cardinal;
+  i:Integer;
+  ret:String;
+  selstr:PWideChar;
+begin
+  Result:='';
+  if win=nil then Exit;
+  sel := win.GetSelection;
+  if sel.IsCollapsed then begin
+    length := win.Frames.Length;
+    if length=0 then Result:=''
+    else begin
+      for i:=0 to length-1 do begin
+        childwin := win.Frames.Item(i);
+        ret:=GetSelectionStrFromWin(childwin);
+        if ret<>'' then begin
+          Result:=ret;
+          exit;
+        end;
+      end;
+    end;
+  end
+  else begin
+    selstr := sel.ToString;
+    Result := WideString(selstr);
+    nsMemory.Free(selstr);
+  end;
+end;
 
 end.
